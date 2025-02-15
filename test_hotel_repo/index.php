c63321a32f053690f0b610e67fecc5c53c31e4df
Zakaria-cpu301 <ramadanzaka27@gmail.com>
2025-02-15 09:10:18 +0700

    beranda website hotel

---
+++ test_hotel_repo/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+</head>
+
+<body>
+    <h1>navbar</h1>
+    <h1>
+        <ul>
+            <li>about </li>
+            <li>contact </li>
+            <li>info </li>
+            <li>login </li>
+            <li>register </li>
+        </ul>
+    </h1>
+</body>
+
+</html>
