29c874e6a35a8af128679e5c3d5db0b6735ec849
Zakaria-cpu301 <ramadanzaka27@gmail.com>
2025-02-15 09:23:37 +0700

    tambah fitur pemesanan kamar hotel

---
+++ test_hotel_repo/pemesanan/form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pesan kamar</title>
+</head>
+
+<body>
+    <center>
+        <h1>FORM PEMESANAN</h1>
+    </center>
+    <form action="#">
+        <label for="name">name</label>
+        <input type="text" name="name" id="name">
+        <label for="address">Address</label>
+        <address id="address"></address>
+        <label for="tel">Telepon</label>
+        <input type="tel" name="tel" id="tel">
+        <button type="submit">Submit</button>
+    </form>
+</body>
+
+</html>
